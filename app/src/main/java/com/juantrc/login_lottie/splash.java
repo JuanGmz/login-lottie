07a01326162ba1daf2fc048fa2c662ed3e59bafe
JuanGmz <juanalfredohd@gmail.com>
2024-09-24 14:37:47 -0600

    Agregando countdowntimer

---
+++ app/src/main/java/com/juantrc/login_lottie/splash.java
@@ -0,0 +1,29 @@
+package com.juantrc.login_lottie;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new CountDownTimer(5000, 1000) {
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                finish();
+            }
+        }.start();
+    }
+}
