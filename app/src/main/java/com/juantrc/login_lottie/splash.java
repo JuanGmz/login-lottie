caa45415f5817a60891ef90911e15bbdb5cf757e
JuanGmz <juanalfredohd@gmail.com>
2024-09-26 20:44:38 -0600

    Splash realizado

--- app/src/main/java/com/juantrc/login_lottie/splash.java
+++ app/src/main/java/com/juantrc/login_lottie/splash.java
@@ -1,7 +1,12 @@
 package com.juantrc.login_lottie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +15,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class splash extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        TextView txtCounter = findViewById(R.id.txtCounter);  // Inicializa el TextView
+
         new CountDownTimer(5000, 1000) {
             public void onTick(long millisUntilFinished) {
-
+                txtCounter.setText("Cargando... " + millisUntilFinished / 1000);
             }
 
             public void onFinish() {
+                Intent intent = new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
                 finish();
             }
         }.start();
